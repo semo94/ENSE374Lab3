0104e39e6008a375cde7046daa0c9509b2db3bf9
Salim Bakri <salimbakri@Salims-MacBook-Pro.local>
2019-10-19 00:08:06 -0600

    creat concrete Chequing class

---
+++ bank/accounts/Chequing.java
@@ -0,0 +1,27 @@
+package bank.accounts;
+
+public class Chequing extends Account {
+
+    /**
+     * Class constructor
+     * @param initialBalance first deposited balance by the user
+     */
+    Chequing(double initialBalance) {
+        super("Chequing", new String[] {"Debit Card", "Cheques", "e-Transfer"}, initialBalance);
+        this.setRunningFees(10);
+    }
+
+    @Override
+    public void withdraw(double withdrawal) {
+        // check if the withdrawal amount exceeds the reserved amount
+        if (this.getCurrentBalance() >= withdrawal) {
+            this.setCurrentBalance(this.getCurrentBalance() - withdrawal);
+        } else {
+            System.out.print("Insufficient fund!");
+        }
+    }
+
+    public void chargeMonthlyFees() {
+        this.setCurrentBalance(this.getCurrentBalance() - this.getRunningFees());
+    }
+}
